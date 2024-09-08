5baf16a522408be2c2ae47e34976a60f7f9e2132
FarhanHaider999 <haiderfarhan178@gmail.com>
2024-09-08 19:12:15 +0500

    ui chang

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,14 +1,12 @@
 export const Header = () => {
   return ( 
-    <div className="fixed inset-x-0 top-3 z-10">
-      <div className="flex justify-center">
-        <nav className="flex flex-col sm:flex-row gap-4 p-2 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-          <a href="#" className="nav-item">Home</a>
-          <a href="#projects" className="nav-item">Projects</a>
-          <a href="#About" className="nav-item">About</a>
-          <a href="#Contact" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
-        </nav>
-      </div>
+    <div className="fixed top-3 left-1/2 transform -translate-x-1/2 z-10">
+      <nav className="flex gap-4 p-2 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+        <a href="#" className="nav-item">Home</a>
+        <a href="#projects" className="nav-item">Projects</a>
+        <a href="#About" className="nav-item">About</a>
+        <a href="#Contact" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
+      </nav>
     </div>
   );
 };
