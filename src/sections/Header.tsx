56f479ce8d2e327ac9ea32d5d0b9eb0f8c0492ab
FarhanHaider999 <haiderfarhan178@gmail.com>
2024-09-09 07:56:59 +0500

    header changes

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,11 @@
 export const Header = () => {
-  return ( 
+  return (
     <div className="fixed top-3 left-1/2 transform -translate-x-1/2 z-10">
-      <nav className="flex gap-4 p-2 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <a href="#" className="nav-item">Home</a>
-        <a href="#projects" className="nav-item">Projects</a>
-        <a href="#About" className="nav-item">About</a>
-        <a href="#Contact" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
+      <nav className="flex gap-2 p-1 sm:gap-4 sm:p-2 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+        <a href="#" className="nav-item text-xs sm:text-sm">Home</a>
+        <a href="#projects" className="nav-item text-xs sm:text-sm">Projects</a>
+        <a href="#about" className="nav-item text-xs sm:text-sm">About</a>
+        <a href="#contact" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900 text-xs sm:text-sm">Contact</a>
       </nav>
     </div>
   );
