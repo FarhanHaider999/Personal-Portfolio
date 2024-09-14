e952dd5cc9fc86dc12d3e4a71fffa14be68cf93e
FarhanHaider999 <haiderfarhan178@gmail.com>
2024-09-14 12:24:24 +0500

    site name changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'], variable: '--font-sans'});
 const calistoga = Calistoga({ subsets: ['latin'], variable: '--font-serif', weight: ['400']});
 
 export const metadata: Metadata = {
-  title: "Portfolio | Farhan Haider",
+  title: "Farhan Haider",
   description: "Created By Farhan Haider",
 };
 
